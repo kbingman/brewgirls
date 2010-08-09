c031f90d50f30ce0d03d649dcfb6fd1cd40a186f
Keith Bingman <keith@keithbingman.com>
2010-08-09 11:30:50 +0200

    added consiistent height

--- test/stories/site_test.rb
+++ test/stories/site_test.rb
@@ -8,16 +8,11 @@ class SiteTest < Test::Unit::TestCase
       assert_contain "Bitch Kitty Racing"
     end 
        
-    scenario "A visitor goes to the testpage" do
-      visit "/test"
-      assert_contain "This is a Test, This is Only a Test"
+    scenario "A visitor goes to the January page" do
+      visit "/months/january"
+      assert_contain "January"
     end  
-    
-    scenario "A visitor goes to the subfolder testpage" do
-      visit "/folder/test"
-      assert_contain "This is a Test of the Subfolder System"
-    end      
-    
+        
     scenario "A visitor should see a 404 for a page that does not exist" do
       visit "/folder/testing"
       assert_contain "Sorry, the page /folder/testing could not be found"
